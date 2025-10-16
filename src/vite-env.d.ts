0b8d00becaa0ca3e5f009a744e1f6298bb305b25
juwavictor <victor@sloovi.com>
2025-10-16 20:57:27 +0100

    chore: add commit linter setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,7 @@
 /// <reference types="vite/client" />
 
 
-declare const AUTO_SW: boolean;
-declare const AUTO_DESTROY_SW: boolean;
-declare const __DATE__: string;
-declare const __SW__: string;
-declare const __SW_SCOPE__: string;
-declare const __SW_TYPE__: string;
+
 
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
