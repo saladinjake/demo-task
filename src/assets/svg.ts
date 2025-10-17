5b0fc23f3ce49671e4cac2e030f25dd44f5dc289
juwavictor <victor@sloovi.com>
2025-10-17 09:41:25 +0100

    chore: svg icons from figma design

--- src/assets/svg.ts
+++ src/assets/svg.ts
@@ -1,5 +1,38 @@
 import CircleAvatar from "./svg/Circle_Avatar.svg";
-
+import Analytics from "./svg/Analytics.svg";
+import App from "./svg/App.svg";
+import Bell from "./svg/Bell.svg";
+import Crm from "./svg/Crm.svg";
+import DropDown from "./svg/DropDown.svg";
+import EmptyBook from "./svg/EmptyBook.svg";
+import ExportDropDown from "./svg/ExportDropDown.svg";
+import Hambugger from "./svg/Hambugger.svg";
+import Home from "./svg/Home.svg";
+import Logo from "./svg/Home.svg";
+import Message from "./svg/Message.svg";
+import QuestionIcon from "./svg/QuestionIcon.svg";
+import Revenue from "./svg/Revenue.svg";
+import SideBar_IconA from "./svg/SideBar_IconA.svg";
+import SideBar_IconB from "./svg/SideBar_IconA.svg";
+import SideBar_IconC from "./svg/SideBar_IconA.svg";
+import SideBar_IconD from "./svg/SideBar_IconA.svg";
 export const Svg = {
   CircleAvatar,
+  Analytics,
+  App,
+  Bell,
+  Crm,
+  DropDown,
+  EmptyBook,
+  ExportDropDown,
+  Hambugger,
+  Home,
+  Logo,
+  Message,
+  QuestionIcon,
+  Revenue,
+  SideBar_IconA,
+  SideBar_IconB,
+  SideBar_IconC,
+  SideBar_IconD,
 };
